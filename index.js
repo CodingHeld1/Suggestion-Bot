e7bcb79d99a1ce428fcc878328f0dc2fb098a02f
CodingHeld1 <102834820+CodingHeld1@users.noreply.github.com>
2022-05-21 20:23:07 +0200

    added staff control 1/2

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const config = require('./config.json')
 
 //Variables 
 let prefix = '-'
-let yes = '<a:rtick:629124378700939315>'
-let no = '<a:rcross:629124462717173781>'
-let temote = '<:pepenotes:905889855567986778>'
+//Lifeboat server emotes
+// let yes = '<a:rtick:629124378700939315>'
+// let no = '<a:rcross:629124462717173781>'
+// let temote = '<:pepenotes:905889855567986778>'
+
+//testing emotes
+//Lifeboat server emotes
+let yes = '<a:rtick:977633786051330078>'
+let no = '<a:rcross:977633814325125210>'
+let temote = '<:pepenotes:977633753688080435>'
 let intro = `To make a suggestion, use ${prefix}suggest (suggestion here). Any other messages do not belong in this channel`
 client.once('ready', () => {
 console.log(`\nPrefix:${prefix} \nName: ${client.user.tag}\n `);
@@ -32,19 +39,36 @@ const staff = client.channels.cache.get("965719796983402536");
     message.channel.send('Test completed!')  
   }
 
-if (command === 'suggestion'){
+if (command === 'suggest'){
 if (!args) return message.channel.send(`You did not provide any suggestion\nCorrect usage of the command: ${prefix}suggest my suggestion`)
 let suggestion = args.join(" ")
 let embed = new MessageEmbed()
-.setTitle(`${temote}New Suggestion`)
+.setTitle(`> ${temote} New Suggestion`)
 .setDescription('Submitter: ' + message.author.tag)
 .addField('Content:', `${suggestion}`)
 .setTimestamp()
 .setFooter({ text: `${message.author.id}`, iconURL: `${message.author.avatarURL()}` });
 const ms = await s_channel.send({ embeds: [embed]})
-const ms2 = await staff.send({embeds:[embed]})
 ms.react(yes)
 .then(() => ms.react(no))
+
+
+
+let embed1 = new MessageEmbed()
+.setTitle(`> ${temote} New Suggestion`)
+.setDescription('Submitter: ' + message.author.tag+`\nReact with ${yes} to approve the suggestion`)
+.addField('Content:', `${suggestion}`)
+.setTimestamp()
+.setFooter({ text: `${message.author.id}`, iconURL: `${message.author.avatarURL()}` });
+const ms2 = await staff.send({embeds:[embed1]})
+ms2.react(yes)
+.then(() => ms2.react(no))
+
+
+
+
+
+
 }
 })
 client.login(config.token)
