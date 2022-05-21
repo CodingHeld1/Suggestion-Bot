fbfbb65128badcaddd89db7b0236e0c9516438b7
CodingHeld1 <102834820+CodingHeld1@users.noreply.github.com>
2022-05-21 14:08:13 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+'use strict';
 const discord = require('discord.js')
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const config = require('./config.json')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
 
 
 //Variables 
@@ -17,5 +20,21 @@ client.user.setPresence({
 })
 
 
+client.on('messageCreate' , message => {
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+  if (!message.guild) return 
+  if (message.author.bot) return;
+const s_channel = client.channels.cache.get("965957179393867786"); //for testing 
+
+  if (command === 'test'){
+    message.channel.send('Test completed!')  
+  }
+
+
+
+
+})
 
 client.login(config.token)
