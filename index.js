179c4f57a0e4cff2dd20f76d394e8864fa7d0c54
CodingHeld1 <102834820+CodingHeld1@users.noreply.github.com>
2022-05-21 13:46:26 +0200

    update

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const discord = require('discord.js')
-
-
-
+const config = require('./config.json')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 
 //Variables 
 let prefix = '!'
 
+client.once('ready', () => {
+console.log(`\nPrefix:${prefix} \nName: ${client.user.tag}\n `);
+client.user.setPresence({
+    status: 'online',
+    activity: {
+        name: 'new suggestions',
+       type: 'WATCHING',
+}})
+})
+
+
+
+client.login(config.token)
